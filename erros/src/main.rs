52dc8dbc2de49367bf383ec171f233e21aacb9da
jypark <jyp8637282@gmail.com>
2026-06-10 22:51:50 +0900

    erros

---
+++ erros/src/main.rs
@@ -0,0 +1,89 @@
+#![allow(warnings)]
+use std::fs::File;
+use std::error::Error;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let greeting_file_result = File::open("hello.txt");
+
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => match error.kind() {
+//             ErrorKind::NotFound => match File::create("hello.txt") {
+//                 Ok(fc) => fc,
+//                 Err(e) => panic!("Problem creating the file: {:?}", e),
+//             },
+//             other_error => {
+//                 panic!("Problem opening the file: {:?}", other_error);
+//             }
+//         },
+//     };
+
+//     let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {:?}", error);
+//             })
+//         } else {
+//             panic!("Problem opening the file: {:?}", error);
+//         }
+//     });
+
+//     let greeting_file = File::open("hello.txt").unwrap();
+
+//     // 보통 unwrap보다 expect를 많이 사용
+//     let greeting_file = File::open("hello.txt")
+//         .expect("hello.txt should be included in this project");
+
+//     let greeting_file = File::open("hello.txt")?;
+
+//     Ok(())
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let username_file_result = File::open("hello.txt");
+
+//     let mut username_file = match username_file_result {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut username = String::new();
+
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username)
+// }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     fs::read_to_string("hello.txt")
+// }
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
+
+fn main() {
+    use std::net::IpAddr;
+
+    let home: IpAddr = "127.0.0.1"
+        .parse()
+        .expect("Hardcoded IP address should be valid");
+}
